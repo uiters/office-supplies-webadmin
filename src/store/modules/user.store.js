b3fc88c42349eafd6be5e258040aa975dbcf2ce6
Trần Đình Thiện <5782349-ThienUIT@users.noreply.gitlab.com>
2020-12-19 16:39:43 +0700

    accounts

--- src/store/modules/user.store.js
+++ src/store/modules/user.store.js
@@ -1,16 +1,41 @@
 import userService from './../../api/user.service'
 
 export default {
-  state: {},
+  state: {
+    _users: [],
+  },
+  getters: {
+    users: state => state._users.map(value => {
+      return {
+        id: value.id,
+        name: value.profile.fullName,
+        phoneNumber: value.profile.phoneNumber,
+        email: value.email,
+        status: value.status,
+      }
+    }),
+  },
+  mutations: {
+    SET_USERS (state, users) {
+      console.log(users)
+      state._users = [...users]
+    },
+  },
   actions: {
-    async getUsers () {
-      const response = await userService.getAllUsers()
-      console.log(response)
+    async getUsers ({ commit }) {
+      try {
+        const response = await userService.getAllUsers()
+        commit(
+          'SET_USERS',
+          response.data.user,
+        )
+      } catch (e) {
+        console.log('cannot get users', e)
+      }
     },
     async getMe () {
       const response = await userService.getMe()
       console.log(response)
     },
   },
-  mutations: {},
 }
